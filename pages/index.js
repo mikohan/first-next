ca40cd266a720bf46f84f3e24d96ba359b9bef71
mikohan <angara99@gmail.com>
2020-10-08 19:12:39 +0300

    added template

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,6 @@
-import Navbar from '../components/Navbar';
-
 export default function Home() {
   return (
     <div>
-      <Navbar />
       <h1>First Next application Change</h1>
     </div>
   );

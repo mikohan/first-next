925bccdb5fa3870a283b1a3372e144f41ee939fb
mikohan <angara99@gmail.com>
2020-10-08 18:45:06 +0300

    added component

---
+++ pages/contacts/index.js
@@ -0,0 +1,12 @@
+import Link from 'next/link';
+
+export default function About() {
+  return (
+    <div>
+      <h1>Contact Page</h1>
+      <Link href="/">
+        <button>Go Home</button>
+      </Link>
+    </div>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,10 @@
-import Head from 'next/head';
-import Link from 'next/link';
-import styles from '../styles/Home.module.css';
+import Navbar from '../components/Navbar';
 
 export default function Home() {
   return (
     <div>
+      <Navbar />
       <h1>First Next application Change</h1>
-      <Link href="/about">About</Link>
     </div>
   );
 }

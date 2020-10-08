d0c30ca58b14d08d08c5ade158302ec9dca8dd01
mikohan <angara99@gmail.com>
2020-10-08 18:30:43 +0300

    About page

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,12 @@
 import Head from 'next/head';
+import Link from 'next/link';
 import styles from '../styles/Home.module.css';
 
 export default function Home() {
   return (
     <div>
       <h1>First Next application Change</h1>
+      <Link href="/about">About</Link>
     </div>
   );
 }
